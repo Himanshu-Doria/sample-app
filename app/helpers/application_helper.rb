8ac630d80bc80a34d3bb326572dd4946d97c7014
Himanshu-Doria <himanshu.doria@moldedbits.com>
2016-06-20 15:40:56 +0530

    Added a full_title helper and Gaurdfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+#A helper is ruby file which contains user_defined methods used for particular controller and views of that controller
+#the application helper is global for the application and can used by any view and controller
+
+	#This method returns the title of the page
+	def full_title(page_title = '')
+		base_title  = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title} "
+		end
+	end
 end
