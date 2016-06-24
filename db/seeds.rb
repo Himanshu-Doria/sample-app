33354d62f5dfe9238614151b6d0acda336a6e61f
Himanshu-Doria <himanshu.doria@moldedbits.com>
2016-06-24 19:08:25 +0530

    Added Admin attribute to user model with delete action available

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,37 @@ User.create!(
 	name: "Example User",
 	email: "example@railstutorial.org",
 	password: "foobarrr",
-	password_confirmation: "foobarrr"
+	password_confirmation: "foobarrr",
+	admin: true
 )
-
+User.create!(
+	name: "Micheal Example",
+	email: "micheal@example.com",
+	password: "password",
+	password_confirmation: "password",
+	admin: true
+)
+User.create!(
+	name: "Sterling Archer",
+	email: "duchess@example.com",
+	password: "password",
+	password_confirmation: "password",
+	admin: true
+)
+User.create!(
+	name: "Lana Kane",
+	email: "hands@example.gov",
+	password: "password",
+	password_confirmation: "password",
+	admin: true
+)
+User.create!(
+	name: "Mallory Archer",
+	email: "boss@example.gov",
+	password: "password",
+	password_confirmation: "password",
+	admin: true
+)
 99.times do |n|
 	name = Faker::Name.name
 	email = "example-#{n + 1}@railstutorial.org"
